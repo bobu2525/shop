52780a8275d91e0fae7959c2265dfb5f50f68fc5
bobu2525 <takadanobuyuki25@gmail.com>
2023-04-25 15:06:38 +0900

    スタッフ入力ホーム作成

--- staff/staff_add.php
+++ staff/staff_add.php
@@ -18,8 +18,18 @@
       <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
    </head>
   <body>
-          スタッフ追加<br>
-            <br>
-            <form method=""></form>
+      スタッフ追加<br>
+      <br>
+      <form method="post" action ="staff_add_check.php">
+          スタッフ名を入力してください<br>
+          <input type="text" name="name" style="width:200px"><br>
+          パスワードを入力してください<br>
+          <input type="text" name="pass" style="width:100px"><br>
+          パスワードをもう一度入力してください念のため本当に最後にもう一度
+          <input type="text" name="pass2" style="width:100px"><br>
+          <br>
+          <input type="button" onclick="history.bock()" value="もどる">
+          <input type="submit" value="OK">
+      </form>
   </body>
 </html>
