c6ddaa121efaa46e5ac15acf56f82e15e754d035
bobu2525 <takadanobuyuki25@gmail.com>
2023-04-29 17:57:12 +0900

    ページ分岐追加

--- staff/staff_branch.php
+++ staff/staff_branch.php
@@ -1,12 +1,37 @@
 
   <?php
+
+if(isset($_POST['add'])==true)
+{
+  header('Location: staff_add.php');
+  exit();
+}
+
 if(isset($_POST['edit'])==true)
 {
-  print'修正ボタンが押したはず';
+  if(isset($_POST['staffcode'])==false)
+  {
+    header('Location:staff_ng.php');
+    exit();
+  }
+
+
+  $staff_code=$_POST['staffcode'];
+  header('Location: staff_edit.php?staffcode='.$staff_code);
+  exit();
 }
 if(isset($_POST['delete'])==true)
 {
-  print'抹消ボタンを押したあなたは・・・';
+  if(isset($_POST['staffcode'])==false)
+  {
+    header('Location: staff_ng.php');
+    exit();
+
+  }
+
+  $staff_code=$_POST['staffcode'];
+  header('Location: staff_delete.php?staffcode='.$staff_code);
+  exit();
 }
 ?>
 
